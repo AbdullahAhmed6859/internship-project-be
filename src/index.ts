1d84c3b1f817c144f07d956fc649df3d2b3c4b7c
Abdullah Ahmed <abdullah.a@systemsltd.com>
2025-05-14 17:10:48 +0500

    Cors configured

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import express from "express";
+import pg from "pg";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+console.log(process.env.DATABASE_URL);
+// const db = new pg.Client(process.env.DATABASE_URL);
+// db.connect();
 
 const app = express();
+
+const corsOptions = {
+  origin: "internship-project-dashboard.netlify.app",
+};
+
+app.use(process.env.ENV !== "PROD" ? cors() : cors(corsOptions));
+// app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).json({
