5e8218e6926f85e5a0a3e1d8eee2743a70d890b9
Abdullah Ahmed <abdullah.a@systemsltd.com>
2025-05-14 15:45:27 +0500

    Express Added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello, world');
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res)=> {
+    res.status(200).json({
+        success: true,
+        message: "hello",
+    });
+});
+
+app.listen(3000);
+console.log('App started on http://localhost:3000');
