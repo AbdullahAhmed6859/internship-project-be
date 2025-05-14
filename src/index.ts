61fc87134b79732b2a7beee7b123b23f8b520c2b
Abdullah Ahmed <abdullah.a@systemsltd.com>
2025-05-14 16:02:52 +0500

    Attempt to run vercel ci/cd

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res)=> {
-    res.status(200).json({
-        success: true,
-        message: "hello",
-    });
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "hello world",
+  });
 });
 
 app.listen(3000);
-console.log('App started on http://localhost:3000');
+console.log("App started on http://localhost:3000");
